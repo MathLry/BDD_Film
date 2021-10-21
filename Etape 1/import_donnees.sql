b8367fdf06ba3046c3695f6d4955a57ae0419869
MathLry <mathiasleroy@laposte.net>
2021-10-21 13:16:11 +0200

    import

--- Etape 1/import_donnees.sql
+++ Etape 1/import_donnees.sql
@@ -124,13 +124,13 @@ VALUES (Film, 2D),
 
 
 INSERT INTO tarif (prix, categorie_de_la_seance, categorie_de_la_place)
-VALUES ('10€', Film, 'A'),
-('12€', Film, 'B'),
-('15€', Film, 'D'),
-('11€', Film, ' B'),
-('14€', Film, 'D'),
-('10€', Film, 'C'),
-('5€', Film, 'A');
+VALUES (10 ||''|| ' €', Film, 'A'),
+(12 ||''|| ' €', Film, 'B'),
+(15 ||''|| ' €', Film, 'D'),
+(11 ||''|| ' €', Film, ' B'),
+(14 ||''|| ' €', Film, 'D'),
+(10 ||''|| ' €', Film, 'C'),
+(5 ||''|| ' €', Film, 'A');
 
 INSERT INTO reservation (nom_spectateur, numero_seance, numero_place)
 VALUES (Calvin, 7, 2),
