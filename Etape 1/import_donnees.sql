ff979f9d12f78893a9eac79d51ee10b1db248286
MathLry <mathiasleroy@laposte.net>
2021-11-02 23:25:03 +0100

    vérification des requests

--- Etape 1/import_donnees.sql
+++ Etape 1/import_donnees.sql
@@ -2,26 +2,23 @@ INSERT INTO acteur (numero_acteur, prenom_acteur, nom_acteur, date_de_naissance,
 VALUES (1, 'Al', 'PACINO', '1940-04-25', 'USA'),
 (2, 'Marlon', 'BRANDO', '1924-04-03', 'USA'),
 (3, 'Robert', 'DE NIRO', '1943-08-17', 'USA'),
-
 (4, 'Mark', 'HAMILL', '1951-09-25', 'USA'),
 (5, 'Carrie', 'FISHER', '1956-10-21', 'USA'),
 (6, 'Harison', 'FORD', '1942-07-13', 'USA'),
-
 (7, 'Zoe', 'SALDANA', '1978-06-19', 'GB'), -- Grande Bretagne
 (8, 'Sam', 'WORTHINGTON', '1976-08-02', 'GB'),
 (9, 'Sigourney', 'WEAVER', '1949-09-08', 'USA'),
-
 (10, 'Michael Andrew', 'FOX', '1961-06-09', 'CAN'), -- Canada
 (11, 'Christopher', 'LLOYD', '1938-10-22', 'USA'),
 (12, 'Lea', 'THOMPSON', '1961-05-31', 'USA'),
-
 (13, 'Kate', 'WINSLET', '1975-10-05', 'GB'),
 (14, 'Leonardo', 'DICAPRIO', '1974-11-11', 'USA'),
 (15, 'Billy', 'ZANE', '1966-02-24', 'USA'),
-
 (16, 'Keanu', 'REEVES', '1964-09-02', 'USA'),
-(17, 'Carrie-Anne', 'Moss', '1967-08-21', 'CAN'),
-(18, 'Laurence', 'FISHBURNE', '1961-07-31', 'USA');
+(17, 'Carrie-Anne', 'MOSS', '1967-08-21', 'CAN'),
+(18, 'Laurence', 'FISHBURNE', '1961-07-31', 'USA'),
+(19, 'Colin', 'FARRELL', '1976-05-31', 'IRL'),
+(20, 'Bridget', 'MOYNAHAN', '1971-04-28', 'USA');
 
 
 INSERT INTO realisateur (numero_realisateur, prenom_realisateur, nom_realisateur, nation_realisateur)
@@ -30,7 +27,8 @@ VALUES (1, 'Francis', 'FORD CAPPOLA', 'USA'),
 (3, 'James', 'Cameron', 'CAN'),
 (4, 'Robert', 'ZEMECKIS', 'USA'),
 (5, 'Lana & Lilly', 'WASHOWSKI', 'USA'),
-(6, 'Richard', 'MARQUAND', 'GB');
+(6, 'Richard', 'MARQUAND', 'GB'),
+(7, 'Roger', 'DONALDSON', 'AU');
 
 
 INSERT INTO film (numero_film, titre_film, date_de_sortie, duree, genre, numero_realisateur) -- format de la date : 'AAAA-MM-JJ'
@@ -40,7 +38,8 @@ VALUES (1, 'Le Parain', '1972-10-18', 175, 'Drame, Mafia, Policier, Criminel', 1
 (4, 'Retour vers le futur', '1985-10-30', 176, 'Comedie, Aventure, Science-Fiction, Fantasy, Romance', 4),
 (5, 'Titanic', '1998-01-07', 194, 'Drame, Catastrophe, Drame historique, Romance', 3),
 (6, 'Matrix', '1999-06-23', 136, 'Action, Aventure, Science-Fiction, Fantasy', 5),
-(7, 'Star Wars VI', '1983-05-12', 134, 'Action, Aventure, Science-Fiction, Fantasy', 6);
+(7, 'Star Wars VI', '1983-05-12', 134, 'Action, Aventure, Science-Fiction, Fantasy', 6),
+(8, 'La Recrue', '2003-01-31', 115, 'Espionnage', 7);
 
 
 INSERT INTO role (nom_du_role, numero_acteur, numero_film)
@@ -70,7 +69,11 @@ VALUES ('Michael Corleone', 1, 1),  -- Le parrain
 
 ('Luke Skywalker', 4, 7),   -- Star Wars VI
 ('Leia Organa', 5, 7),
-('Han Solo', 6, 7);
+('Han Solo', 6, 7),
+
+('Walter Burke', 1, 8),   -- La Recrue
+('James Clayton', 19, 8),
+('Layla Moore', 20, 8);
 
 
 INSERT INTO place (numero_place, categorie_de_la_place)
@@ -105,16 +108,28 @@ INSERT INTO categorie_place (categorie_de_la_place, type_place)
 VALUES ('A', 'Plein tarif'),
 ('B', 'Tarif reduit'),
 ('C', 'Tarif etudiant'),
-('D', 'Tarif moins de 16 ans');
+('D', 'Tarif moins de 16 ans'),
+('E', 'Catégorie spéciale');
 
 
 INSERT INTO seance (numero_seance, date_de_la_seance, horaire, numero_film, categorie_de_la_seance) -- format de l'horaire : 'hh:mm:ss'
-VALUES (9, '2008-10-18', '13:10:00', 1,1),
-(7, '2009-08-20', '23:20:00', 2,2),
-(3, '2012-02-21', '19:30:00', 3,1),
-(43, '2015-10-30', '10:00:00', 4,3),
-(16, '2020-01-07', '14:40:00', 5,5),
-(23, '2017-06-23', '16:30:00', 6,4);
+VALUES (9, '2008-10-18', '13:10:00', 1, 1),
+(7, '2009-08-20', '23:20:00', 2, 2),
+(3, '2012-02-21', '19:30:00', 3, 1),
+(12, '2012-02-23', '13:30:00', 3, 1),
+(43, '2015-10-30', '10:00:00', 4, 3),
+(16, '2020-01-07', '14:40:00', 5, 5),
+(23, '2017-06-23', '16:30:00', 6, 4),
+(25, '2016-12-14', '11:50:00', 7, 1),
+(18, '2017-06-23', '16:30:00', 2, 2),
+(13, '2016-12-16', '19:00:00', 7, 3),
+(6, '2016-12-16', '11:00:00', 7, 3),
+(2, '2016-12-16', '09:00:00', 7, 3),
+(17, '2016-12-16', '21:30:00', 7, 3),
+(8, '2016-12-16', '23:00:00', 7, 3),
+(4, '2003-06-11', '14:30:00', 8, 4),
+(1, '2003-06-11', '16:00:00', 8, 3),
+(11, '2016-12-18', '18:30:00', 7, 5);
 
 
 INSERT INTO categorie_seance (categorie_de_la_seance, type_seance)
@@ -139,17 +154,22 @@ VALUES (11.00, 1, 'A'),
 (13.20, 3, 'A'),
 (11.20, 3, 'B'),
 (8.60, 3, 'C'),
-(7.50, 2, 'D'),
+(7.50, 3, 'D'),
 
 (11.10, 4, 'A'),
 (9.10, 4, 'B'),
 (7.10, 4, 'C'),
-(6.00, 2, 'D'),
+(6.00, 4, 'D'),
 
 (11.05, 5, 'A'),
 (9.05, 5, 'B'),
-(7.05, 2, 'C'),
-(5.95, 5, 'D');
+(7.05, 5, 'C'),
+(5.95, 5, 'D'),
+
+(10.99, 1, 'E'),
+(8.99, 1, 'E'),
+(7.99, 1, 'E'),
+(5.89, 1, 'E');
 
 
 INSERT INTO reservation (nom_spectateur, numero_seance, numero_place)
--- Etape 2/Requetes_clients.sql
+++ Etape 2/Requetes_clients.sql
@@ -1,7 +1,7 @@
--- requete n°1
+-- requete n°1	(OK)
 SELECT DISTINCT seance.date_de_la_seance 
 FROM seance
-INNER JOIN seance ON seance.numero_film = film.numero_film
+INNER JOIN film ON seance.numero_film = film.numero_film
 INNER JOIN role ON role.numero_film = film.numero_film
 INNER JOIN acteur ON acteur.numero_acteur = role.numero_acteur
 WHERE numero_acteur IN (
@@ -9,15 +9,15 @@ WHERE numero_acteur IN (
     WHERE numero_acteur = 1
 )
 
--- requete n°2
+-- requete n°2	(OK)
 SELECT film.titre_film AS list_film, prix, horaire
 FROM film
 INNER JOIN seance ON seance.numero_film = film.numero_film
 INNER JOIN tarif ON tarif.categorie_de_la_seance = seance.categorie_de_la_seance
 WHERE seance.horaire >= '18:00:00' AND tarif.prix <= '10'
 
--- requete n°3
-SELECT role.nom_du_role
+-- requete n°3	(OK)
+SELECT acteur.numero_acteur, role.nom_du_role
 FROM role
 INNER JOIN acteur ON role.numero_acteur = acteur.numero_acteur
 WHERE acteur.numero_acteur = (
@@ -27,27 +27,58 @@ WHERE acteur.numero_acteur = (
 )
 ORDER BY role.nom_du_role ASC
 
--- requete n°4
-SELECT seance.date_de_la_seance AS date, seance.horaire AS horaire
+-- requete n°4	(OK)
+SELECT film.numero_film, seance.date_de_la_seance AS date, seance.horaire AS horaire
 FROM seance
 INNER JOIN film ON film.numero_film = seance.numero_film
 WHERE film.numero_film = 7
-ORDER BY seance.horaire ASC, seance.date_de_la_seance DESC
+ORDER BY seance.date_de_la_seance DESC, seance.horaire ASC
 
--- requete n°5
+-- requete n°5	(OK)
 SELECT COUNT(DISTINCT nom_acteur) AS nombre_acteur
 FROM acteur
 
--- requete n°6
+-- requete n°6	(OK)
+SELECT titre_film, COUNT(film.numero_film) AS 'Séances programmées'
+FROM film
+INNER JOIN seance ON film.numero_film = seance.numero_film
+GROUP BY film.numero_film
+HAVING COUNT(film.numero_film) = 2
 
+-- requete n°7 (OK)
+SELECT acteur.nom_acteur AS Acteur, realisateur.prenom_realisateur || ' ' || realisateur.nom_realisateur AS Réalisateur
+FROM realisateur
+INNER JOIN film ON realisateur.numero_realisateur = film.numero_realisateur
+INNER JOIN role ON film.numero_film = role.numero_film
+INNER JOIN acteur ON role.numero_acteur = acteur.numero_acteur
+WHERE realisateur.numero_realisateur IN (
+	film.numero_realisateur
+)
+ORDER BY Acteur ASC
 
--- requete n°7
+-- requete n°8	(OK)
+SELECT prenom_acteur || ' ' || nom_acteur AS Acteur, COUNT(role.numero_acteur) AS 'Nombre de rôle réalisé'
+FROM acteur
+INNER JOIN role ON role.numero_acteur = acteur.numero_acteur
+GROUP BY role.numero_acteur
+ORDER BY COUNT(role.numero_acteur) DESC
 
+-- requete n°9	(A REVOIR)
+CREATE VIEW IF NOT EXISTS liste_film_acteur AS
+SELECT prenom_acteur || ' ' || nom_acteur AS Acteur, COUNT(seance.numero_seance) AS Nombre de séances
+FROM acteur
+INNER JOIN role ON role.numero_acteur = acteur.numero_acteur
+INNER JOIN film ON film.numero_film = role.numero_film
+INNER JOIN seance ON film.numero_film = seance.numero_film
+WHERE film.numero_film = role.numero_film;
 
--- requete n°8
-
-
--- requete n°9
-
-
--- requete n°10
+-- requete n°10	(A REVOIR)
+CREATE TRIGGER IF NOT EXISTS trigger_categorie_speciale
+UPDATE ON reservation
+INNER JOIN place ON place.numero_place = reservation.numero_place
+INNER JOIN categorie_place ON place.categorie_de_la_place = categorie_place.categorie_de_la_place
+WHEN (reservation.numero_place = 1 AND NOT NULL)
+BEGIN
+	reservation.catégorie_de_la_place := :'E'	-:'D'
+	DBMS_OUTPUT.PUT_LINE('La catégorie de la place 1 est maintenant en :' || reservation.categorie_de_la_place);
+END;
